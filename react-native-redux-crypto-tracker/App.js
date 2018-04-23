5fb2f62bdb4ef62ccdc7b5642fd348fd31587c10
Shiv Gupta <shiv@Shivs-MacBook-Pro.local>
2018-04-23 00:15:53 -0400

    adding redux to app

--- react-native-redux-crypto-tracker/App.js
+++ react-native-redux-crypto-tracker/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {Header} from './src/components';
+import {Provider} from 'react-redux';
+
+import Store from './src/Store';
+import {Header, CryptoContainer} from './src/components';
 
 
 export default class App extends React.Component {
   render() {
     return (
+      <Provider store = {Store}>
       <View style={styles.container}>
         <Header />
+        <CryptoContainer />
       </View>
+      </Provider>
     );
   }
 }
